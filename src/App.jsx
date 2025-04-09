ea94a733fd4e8c601fcb1737cce78a6d5c994102
lizzyjoo <jejviolinist@gmail.com>
2025-04-09 15:50:20 -0400

    Add basic application structure and entry points

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BrainConnectivityApp from "./components/BrainConnectivityApp";
+import "./App.css";
+import "./components/BrainConnectivity.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrainConnectivityApp />
+    </div>
+  );
+}
+
+export default App;
